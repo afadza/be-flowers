89b84e3083b8403182e4f21bc83950e2459ad9f2
afadz <arya.skoba@gmail.com>
2024-01-03 19:52:06 +0700

    online

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,15 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "ep-lingering-bonus-71103436.ap-southeast-1.aws.neon.tech",
   port: 5432,
-  username: "postgres",
-  password: "root",
+  username: "arya.skoba",
+  password: "7cqTgyOX2Udj",
   database: "be-flowers",
   synchronize: true,
   logging: false,
   entities: ["src/entity/*.ts"],
   migrations: ["src/migration/**/*.ts"],
   subscribers: [],
+  ssl: true,
 });
